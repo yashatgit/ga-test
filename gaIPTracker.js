58cec38e635fccb58cc2f99e6c6d9e0a6ab365ea
yashSoni <mailyash89@gmail.com>
2018-03-01 13:51:39 +0530

    changes

--- gaIPTracker.js
+++ gaIPTracker.js
@@ -1,22 +1,27 @@
 var sampleResponse = {
-  cookieId: "97b382ee-7ef5-4fc1-b99b-df88f7eecd90",
-  ipAddress: "14.141.149.46",
   name: "Sprinklr, Inc.",
   website: "sprinklr.com",
-  street: "29 West 35th Street 8th Floor",
-  city: "New York",
-  regionShort: "NY",
-  postal: "10001",
-  countryShort: "US",
-  country: "United States",
-  phone: "917-933-7800",
   employees: "2,500 to 5,000",
   revenue: "$250,000,000 to $500,000,000",
+  
+  city: "New York",
+  regionShort: "NY",
+  country: "United States",
+  
   sicGroup: "Business Services",
-  category: "Computer Related Services, Nec",
-  sicCode: "7379",
   naicsGroup: "Professional, Scientific, and Technical Services",
+  category: "Computer Related Services, Nec",
   category2: "Other Computer Related Services",
+  confidence: "77",
+  
+  
+  countryShort: "US",
+  cookieId: "97b382ee-7ef5-4fc1-b99b-df88f7eecd90",
+  ipAddress: "14.141.149.46",
+  street: "29 West 35th Street 8th Floor",
+  postal: "10001",
+  phone: "917-933-7800",
+  sicCode: "7379",
   naicsCode: "541519",
   latitude: 40.7499318,
   longitude: -73.9854829,
@@ -24,15 +29,15 @@ var sampleResponse = {
   facebook: "sprinklr",
   twitter: "sprinklr",
   linkedIn: "399351",
-  confidence: "77",
   isISP: "0"
 };
 
 var trackerToGAFieldMapper = {
-  name: "dimension1",
-  revenue: "dimension2",
-  country: "dimension3",
-  sicGroup: "dimension4"
+  website: "dimension1",
+  name: "dimension2",
+  revenue: "dimension3",
+  country: "dimension4",
+  sicGroup: "dimension5",
 };
 var mapResponseToGACF = function(response) {
   if (!response) {
